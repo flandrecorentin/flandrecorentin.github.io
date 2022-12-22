bf9d72a61a2be5cfaac42965aacb83426b9ad1e8
Flandre Corenitn <flandrecorentin@gmail.com>
2022-12-23 00:08:38 +0100

    - suite Homepage avec les couleurs pour l'algo

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -3,10 +3,11 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
-  styleUrls: ['./section.component.css']
+  styleUrls: ['./section.component.css'],
 })
 export class SectionComponent {
   @Input() description: string | undefined;
   @Input() title: string | undefined;
+  @Input() linkSVG: string = "";
   // same thing for the svg
 }
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -14,7 +14,7 @@ export class HomePageComponent {
   styleClassRight = "route-label route-label-right";
   styleClassDivLeft = "route-label-div-left";
   styleClassDivRight = "route-label-div-right";
-  curriculumInfos = ['Cursus universitaire et professionel', 'blablablablablabla'];
+  curriculumInfos = ['Cursus universitaire et professionel', 'blablablablablabla', 'curriculum'];
   projectsInfos = ['Projets informatiques (Projets persos, universitaires ou professionel)', 'blablablablablabla'];
   sportInfos = ['Ma pratique de sport', 'blablablablablabla'];
   artInfos = ['Cursus universitaire et professionel', 'blablablablablabla'];
